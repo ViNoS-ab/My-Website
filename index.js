a6692bc4ac1ae84f7527e1b39a203b148c3276d3
ViNoS Ben <vinos.ben@gmail.com>
2020-09-09 20:10:16 +0100

    nothing actullay

--- index.js
+++ index.js
@@ -67,7 +67,6 @@ app.post("/api/posts", cors(corsOptions), (req, res) => {
   res.send(post);
   fs.writeFile("./data/posts.json", json, () => {
     console.log(JSON.stringify(post));
-    console.log(last(json));
   });
 });
 
